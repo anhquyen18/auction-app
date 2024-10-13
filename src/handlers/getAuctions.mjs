aa125b929678cb60ac2222a563f472524d8f5a15
anhquyen18 <anhquyen18092000@gmail.com>
2024-10-13 17:54:36 +0700

    validate status param on getAuctions

--- src/handlers/getAuctions.mjs
+++ src/handlers/getAuctions.mjs
@@ -1,18 +1,30 @@
 import AWS from "aws-sdk";
 import commonMiddleware from "../lib/commonMiddleware.mjs";
 import createError from "http-errors";
+import validatorMiddleware from "@middy/validator";
+import getAuctionsSchema from "../lib/schemas/getAuctionsSchema.mjs";
+import { transpileSchema } from "@middy/validator/transpile";
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function getAuctions(event, context) {
+  const { status } = event.queryStringParameters;
   let auctions;
 
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    IndexName: "statusAndEndDate",
+    KeyConditionExpression: "#status = :status",
+    ExpressionAttributeValues: {
+      ":status": status,
+    },
+    ExpressionAttributeNames: {
+      "#status": "status",
+    },
+  };
+
   try {
-    const result = await dynamodb
-      .scan({
-        TableName: process.env.AUCTIONS_TABLE_NAME,
-      })
-      .promise();
+    const result = await dynamodb.query(params).promise();
 
     auctions = result.Items;
   } catch (error) {
@@ -26,4 +38,9 @@ async function getAuctions(event, context) {
   };
 }
 
-export const handler = commonMiddleware(getAuctions);
+export const handler = commonMiddleware(getAuctions).use(
+  validatorMiddleware({
+    eventSchema: transpileSchema(getAuctionsSchema),
+    useDefaults: true,
+  })
+);
